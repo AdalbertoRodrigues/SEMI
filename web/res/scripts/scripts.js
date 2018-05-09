4dd89f015b6f321a5135c7ea3d5ddf9424dc54a0
ViniciusLima5 <vinicius.lalmeida98@gmail.com>
2018-05-09 01:55:16 -0300

    Tabela de Histórico do Motorista

--- web/res/scripts/scripts.js
+++ web/res/scripts/scripts.js
@@ -1335,6 +1335,23 @@ app.controller("menuMotoristaViagemController", function ($scope, $rootScope, da
         $rootScope.getViagemAtual();
         $rootScope.getHistoricoViagem();
     });
+    $scope.updateStatus = function () {
+        $scope.status_viagem = $("#form-admin-status").val();
+        $http({
+                method: 'POST',
+                url: ctx + '/viagem.jsp?action=updateStatus&idViagem=' + $scope.viagemAtual.id + '&status='+ $scope.status_viagem
+            }).then(function successCallback(response) {
+                if (response.data.resposta == "SUCCESS") {
+                    alert($scope.status_viagem);
+                } else {
+                    alert("Ocorreu um erro ao alterar o status");
+                }
+
+            }, function errorCallback(response) {
+                console.log('Error');
+            });
+       
+    };
     $scope.abrirModalChat = function () {
         $('#modal-chat').modal('toggle');
     };
